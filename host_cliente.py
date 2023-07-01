7832047273154bbdee237e76e8ab025243ef705d
nicoph <56656781+nicoph@users.noreply.github.com>
2023-07-01 19:03:33 -0300

    Entrega

--- host_cliente.py
+++ host_cliente.py
@@ -1,20 +1,216 @@
 import requests
 
-servidor_url = "http://localhost:5000"
+servidor_url = ""
+
+def obtener_ip_servidor():
+    default="localhost"
+    global servidor_url  
+    servidor_url = input("Ingrese la IP del servidor(Enter para localhost): ")
+    if servidor_url == "":
+        servidor_url=default
+    print("Conexión establecida con el servidor:", servidor_url)
 
 def obtener_estudiantes():
-    response = requests.get(f"{servidor_url}/estudiantes")
+    response = requests.get(f"http://{servidor_url}:5000/estudiantes")
     if response.status_code == 200:
-        return response.json()
+        estudiantes = response.json()
+        if estudiantes:
+            for estudiante in estudiantes["estudiantes"]:
+                legajo = estudiante["legajo"]
+                nombre = estudiante["nombre"]
+                apellido = estudiante["apellido"]
+                comision = estudiante["comision"]
+                nota = estudiante["nota"]
+                print(f"Legajo: {legajo}, Nombre: {nombre}, Apellido: {apellido}, Comisión: {comision}, Nota: {nota}")
+        
     else:
-        return []
+        print("Error al obtener los estudiantes.")
 
-def agregar_estudiante(estudiante):
-    response = requests.post(f"{servidor_url}/estudiantes", json=estudiante)
+def consultar_estudiante_por_legajo():
+    legajo = input("Ingrese el legajo del estudiante: ")
+    response = requests.get(f"http://{servidor_url}:5000/estudiantes/legajo/{legajo}")
+    if response.status_code == 200:
+        estudiantes = response.json()
+        if estudiantes:
+                legajo = estudiantes["legajo"]
+                nombre = estudiantes["nombre"]
+                apellido = estudiantes["apellido"]
+                comision = estudiantes["comision"]
+                nota = estudiantes["nota"]
+                print(f"Legajo: {legajo}, Nombre: {nombre}, Apellido: {apellido}, Comisión: {comision}, Nota: {nota}")
+        else:
+            print("No se encontraron estudiantes con ese legajo.")
+    else:
+        print("Error al obtener los estudiantes.")
+
+def consultar_estudiante_por_apellido():
+    apellido = input("Ingrese el apellido del estudiante: ")
+    response = requests.get(f"http://{servidor_url}:5000/estudiantes/apellido/{apellido}")
+    if response.status_code == 200:
+        estudiantes = response.json()
+        if estudiantes:
+            for estudiante in estudiantes:
+                legajo = estudiante["legajo"]
+                nombre = estudiante["nombre"]
+                apellido = estudiante["apellido"]
+                comision = estudiante["comision"]
+                nota = estudiante["nota"]
+                print(f"Legajo: {legajo}, Nombre: {nombre}, Apellido: {apellido}, Comisión: {comision}, Nota: {nota}")
+        else:
+            print("No se encontraron estudiantes.")
+    else:
+        print("Error al obtener los estudiantes.")
+
+def consultar_notas():
+    response = requests.get(f"http://{servidor_url}:5000/notas")
+    if response.status_code == 200:
+        data = response.json()
+        print("Estadísticas de notas:")
+        print(f"Mediana: {data['mediana']}")
+        print(f"Media: {data['media']}")
+        print(f"Q1: {data['q1']}")
+        print(f"Q3: {data['q3']}")
+    else:
+        print("Error al obtener los estudiantes y las estadísticas.")
+
+def agregar_estudiante():
+    nombre = input("Ingrese el nombre del estudiante: ")
+    apellido = input("Ingrese el apellido del estudiante: ")
+    comision = input("Ingrese la comisión del estudiante: ")
+    nota = input("Ingrese la nota del estudiante: ")
+    estudiante = {
+        "nombre": nombre,
+        "apellido": apellido,
+        "comision": comision,
+        "nota": nota
+    }
+    response = requests.post(f"http://{servidor_url}:5000/estudiantes", json=estudiante)
     if response.status_code == 201:
-        return response.json()
+        estudiante_creado = response.json()
+        print("Estudiante agregado:", estudiante_creado)
     else:
-        return None
+        print("Error al agregar el estudiante.")
 
-# Agregar funciones para actualizar y eliminar estudiantes
+def borrar_estudiante():
+    legajo = input("Ingrese el legajo del estudiante a borrar: ")
+    response = requests.delete(f"http://{servidor_url}:5000/estudiantes/borrar/{legajo}")
+    if response.status_code == 200:
+        print("Estudiante borrado exitosamente.")
+    else:
+        print("Error al eliminar el estudiante")
 
+def ver_aprobados():    
+    response = requests.get(f"http://{servidor_url}:5000/estudiantes/aprobado")
+    if response.status_code == 200:
+        estudiantes = response.json()
+        if estudiantes:
+            for estudiante in estudiantes:
+                legajo = estudiante["legajo"]
+                nombre = estudiante["nombre"]
+                apellido = estudiante["apellido"]
+                comision = estudiante["comision"]
+                nota = estudiante["nota"]
+                print(f"Legajo: {legajo}, Nombre: {nombre}, Apellido: {apellido}, Comisión: {comision}, Nota: {nota}")
+        else:
+            print("No se encontraron estudiantes.")
+    else:
+        print("Error al obtener los estudiantes.")
+
+def ver_desaprobados():    
+    response = requests.get(f"http://{servidor_url}:5000/estudiantes/desaprobado")
+    if response.status_code == 200:
+        estudiantes = response.json()
+        if estudiantes:
+            for estudiante in estudiantes:
+                legajo = estudiante["legajo"]
+                nombre = estudiante["nombre"]
+                apellido = estudiante["apellido"]
+                comision = estudiante["comision"]
+                nota = estudiante["nota"]
+                print(f"Legajo: {legajo}, Nombre: {nombre}, Apellido: {apellido}, Comisión: {comision}, Nota: {nota}")
+        else:
+            print("No se encontraron estudiantes.")
+    else:
+        print("Error al obtener los estudiantes.")
+
+def ver_comision():
+    nro=input("Ingrese la Comision: ")    
+    response = requests.get(f"http://{servidor_url}:5000/estudiantes/comision/{nro}")
+    if response.status_code == 200:
+        estudiantes = response.json()
+        if estudiantes:
+            for estudiante in estudiantes:
+                legajo = estudiante["legajo"]
+                nombre = estudiante["nombre"]
+                apellido = estudiante["apellido"]
+                comision = estudiante["comision"]
+                nota = estudiante["nota"]
+                print(f"Legajo: {legajo}, Nombre: {nombre}, Apellido: {apellido}, Comisión: {comision}, Nota: {nota}")
+        else:
+            print("No se encontraron estudiantes.")
+    else:
+        print("Error al obtener los estudiantes.")
+
+def mostrar_menu():
+    print("\n")
+    print("----- Menú de Opciones -----")
+    print("1. Recuperar lista de estudiantes")
+    print("2. Consultar estudiante por legajo")
+    print("3. Consultar estudiante por apellido")
+    print("4. Consultar notas")
+    print("5. Agregar estudiante")
+    print("6. Borrar estudiante")
+    print("7. Ver Aprobados")
+    print("8. Ver desaprobados")
+    print("9. Ver comision")
+    print("0. Salir")
+    print("-----------------------------")
+    print("\n")
+
+
+
+def main():
+    obtener_ip_servidor()
+
+    while True:
+        mostrar_menu()
+        opcion = input("Ingrese una opción: ")
+
+        if opcion == "1":
+            estudiantes = obtener_estudiantes()
+            if estudiantes:
+                for estudiante in estudiantes:
+                    print(estudiante)
+            else:
+                print("No se encontraron estudiantes.")
+
+        elif opcion == "2":
+            consultar_estudiante_por_legajo()
+
+        elif opcion == "3":
+            consultar_estudiante_por_apellido()
+
+        elif opcion == "4":
+            consultar_notas()
+
+        elif opcion == "5":
+            agregar_estudiante()
+
+        elif opcion == "6":
+            borrar_estudiante()
+        elif opcion == "7":
+            ver_aprobados()
+        elif opcion == "8":
+            ver_desaprobados()
+        elif opcion == "9":
+            ver_comision()
+
+        elif opcion == "0":
+            print("Hasta luego!")
+            break
+
+        else:
+            print("Opción inválida. Intente nuevamente.")
+    
+if __name__ == "__main__":
+    main()
